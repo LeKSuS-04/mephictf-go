90675f73ad7dbeb68811508e34eb3f94590cc4e9
taranda.dv <taranda.dv@dozen.mephi.ru>
2024-11-19 21:24:31 +0300

    hw_1

--- lrucache/lrucache.go
+++ lrucache/lrucache.go
@@ -2,25 +2,35 @@ package lrucache
 
 type LruCache struct {
 	// Add your fields here
+	Capacity int
+	elements map[int]int
 }
 
 // Creates a new LruCache with the given capacity.
 func New(capacity int) *LruCache {
-	return nil
+
+	return &LruCache{Capacity: capacity, elements: make(map[int]int)}
 }
 
 // Get returns value associated with the key.
 //
-// The second value is a bool that is true if the key exists in the cache,
+// The second value is a bool t	ey exists in the cache,
 // and false if not.
 func (l *LruCache) Get(key int) (int, bool) {
-	return 0, false
+	value, exists := l.elements[key]
+	return value, exists
 }
 
 // Set updates value associated with the key.
 //
 // If there is no key in the cache new (key, value) pair is created.
 func (l *LruCache) Set(key, value int) {
+	_, oke := l.elements[key]
+	if oke {
+		l.elements[key] = value
+	} else {
+		l.elements[key] = value
+	}
 }
 
 // Range calls function f on all elements of the cache
